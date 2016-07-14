24dc20ec8fd5f94fdec2e17a883a1f3290780435
Dimitry <dvporg@gmail.com>
2016-07-14 21:48:14 +0200

    better time in chat, migration improvement

--- db/migrate/20160714162133_create_chats.rb
+++ db/migrate/20160714162133_create_chats.rb
@@ -1,9 +1,11 @@
 class CreateChats < ActiveRecord::Migration[5.0]
   def change
-    create_table :chats do |t|
-      t.string :user_name
-      t.text :message
-      t.timestamps
+    unless table_exists? :chats
+      create_table :chats do |t|
+        t.string :user_name
+        t.text :message
+        t.timestamps
+      end
     end
   end
 end
